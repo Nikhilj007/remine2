9b75f834f9cdce891451772c1137f37984cc9676
Nikhil Jha <nikhiljhanov15@gmail.com>
2023-04-22 00:42:17 +0530

    update

--- client/src/components/Achievements.jsx
+++ client/src/components/Achievements.jsx
@@ -5,13 +5,14 @@ import { motion } from 'framer-motion'
 import AchievementCard from './AchievementCard'
 
 const Achievements = () => {
-    const achieveList = {
+    const achieveList = [
         {heading:'Got registered officially' ,detail:' Remine India Private Limited is a Private incorporated on 06 December 2022 and is classified as the Indian Non-Government Company and is registered at Registrar of Companies, Bangalore.'},
         {heading:'Recycled x number of lithium batteries',detail: 'We have recycled very high amount of lithium batteries with ease and extracting profit out of the garbage batteries.'},
         {heading:'Got registered officially' ,detail:' Remine India Private Limited is a Private incorporated on 06 December 2022 and is classified as the Indian Non-Government Company and is registered at Registrar of Companies, Bangalore.'},
         {heading:'Recycled x number of lithium batteries', detail:'We have recycled very high amount of lithium batteries with ease and extracting profit out of the garbage batteries.'},
 
-}
+    ]
+    
   return (
     <>
         <div className='my-30 text-3xl flex flex-col'>
